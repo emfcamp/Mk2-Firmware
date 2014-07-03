f75468d3d551ed6d129f1ffa3f64c9f00872f670
Marek <mail@marekventur.de>
2014-07-03 16:56:26 +0100

    Enable xTimerPendFunctionCall

--- hardware/emfcamp/sam/libraries/FreeRTOS_ARM/utility/FreeRTOSConfig.h
+++ hardware/emfcamp/sam/libraries/FreeRTOS_ARM/utility/FreeRTOSConfig.h
@@ -1,5 +1,5 @@
 /*
-    FreeRTOS V8.0.0 - Copyright (C) 2014 Real Time Engineers Ltd. 
+    FreeRTOS V8.0.0 - Copyright (C) 2014 Real Time Engineers Ltd.
     All rights reserved
 
     VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
@@ -136,6 +136,7 @@ to exclude the API function. */
 #define INCLUDE_vTaskSuspend			1
 #define INCLUDE_vTaskDelayUntil			1
 #define INCLUDE_vTaskDelay				1
+#define INCLUDE_xTimerPendFunctionCall  1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
