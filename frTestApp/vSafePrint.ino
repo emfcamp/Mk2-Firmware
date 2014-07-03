d69d463ca94802b2457371cede8b790e202df1ad
Marek <mail@marekventur.de>
2014-07-03 17:01:26 +0100

    Remove test app

--- frTestApp/vSafePrint.ino
+++
@@ -1,7 +0,0 @@
-void vSafePrint(String text) {
-    // Don't wait forever - That could lead to deadlocks
-    if (xSemaphoreTake(xSerialMutex, 10) == pdTRUE) {
-        Serial.println(text);
-        xSemaphoreGive(xSerialMutex);
-    }
-}
