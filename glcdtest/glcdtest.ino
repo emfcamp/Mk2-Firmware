9ca3b75524f4dcc713ff5a2cc22d605577699b8e
dpslwk <dps.lwk@gmail.com>
2014-08-13 09:01:43 +0100

    Animate test to prove it works more than once

--- glcdtest/glcdtest.ino
+++ glcdtest/glcdtest.ino
@@ -18,9 +18,29 @@ void setup() {
   GLCD.Init(NON_INVERTED);  
   GLCD.DrawBitmap(logo, 0,  (64-46)/2); 
   GLCD.display();
+  delay(500);
 }
- 
+
+int phase = 0;
 void loop() {
   // put your main code here, to run repeatedly:
- 
+  if (phase==0) {
+     GLCD.ClearScreen();
+     GLCD.FillRect(0,0,128,8);
+     GLCD.FillRect(0,16,128,8);
+     GLCD.FillRect(0,32,128,8);
+     GLCD.FillRect(0,48,128,8);
+     GLCD.display();
+     phase=1;
+   } else {
+     GLCD.ClearScreen();
+     GLCD.FillRect(0,8,128,8);
+     GLCD.FillRect(0,24,128,8);
+     GLCD.FillRect(0,40,128,8);
+     GLCD.FillRect(0,56,128,8);
+     GLCD.display();
+     phase=0;
+   }
+   delay(500);
+   
 }
