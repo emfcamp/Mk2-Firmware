8cc3110b9bf0b2d65540398183450f1846f2a960
Mark Steward <marksteward@gmail.com>
2014-09-14 23:12:10 +0100

    Remove unused GLCD tests

--- glcdtest/allBitmaps.h
+++
@@ -1,5 +0,0 @@
-/* allBitmaps.h bitmap header for GLCD library */
-/* This file is created automatically by the glcdMakeBitmap utility */
-/* Any edits to this file will be lost when glcdMakeBitmap is next run */
-
-#include "logo.h"
